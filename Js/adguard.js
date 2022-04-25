c0da70930dd6034f2649b018c9206e1bd8613f95
DevCoz <qq851428876@gmail.com>
2022-04-25 09:14:05 +0800

    Update adguard.js

--- Js/adguard.js
+++ Js/adguard.js
@@ -1,3 +1,12 @@
+/*
+AdGuard Pro
+
+[MITM]
+hostname = mobile-api.adguard.com
+
+[Script]
+AdGuard.js = type=http-response,pattern=^https:\/\/mobile-api\.adguard\.com\/api\/1\.0\/ios_validate_receipt$,requires-body=1,max-size=0,script-path=https://github.com/DevCoz/Ruleset/raw/Pv/Js/adguard.js,script-update-interval=0
+*/
 obj = {
     "products": [{
         "product_id": "com.adguard.lifetimePurchase",
