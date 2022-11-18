0eb1c1609c0a98f652d8c578992d6709b44b9bb6
DevCoz <qq851428876@gmail.com>
2022-11-18 11:01:25 +0800

    Create redditAD.js

---
+++ Reddit/redditAD.js
@@ -0,0 +1,25 @@
+//
+if ($response.body.includes("adEvents")) {
+    var body = JSON.parse($response.body || null);
+    if (body.data.subredditInfoByName) {
+        removeAD(body.data.subredditInfoByName.elements)
+        $done({ body: JSON.stringify(body) });
+    } else if (body.data.home) {
+        removeAD(body.data.home.elements)
+        $done({ body: JSON.stringify(body) });
+    } else if (body.data.popular) {
+        removeAD(body.data.popular.elements)
+        $done({ body: JSON.stringify(body) });
+    } else if (body.data.children && body.data.children.commentsPageAds) {
+        body.data.children.commentsPageAds = []
+        $done({ body: JSON.stringify(body) });
+    } else {
+        $done({});
+    }
+} else {
+    $done({});
+}
+
+function removeAD(elements) {
+    elements.edges = elements.edges.filter((n) => !n.node.adEvents)
+}
