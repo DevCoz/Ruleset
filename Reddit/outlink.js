e3d26aabf3c08bf6fda8d3187c06dbc472fdb185
RoboCat <qq851428876@gmail.com>
2024-03-28 11:16:31 +0800

    Delete Reddit directory

--- Reddit/outlink.js
+++
@@ -1,9 +0,0 @@
-var url = $request.url.replace(/^https?:\/\/out.reddit.com\/.*url=(https?[\w%\.\(\)\-]+).*/g, "$1")
-var modifiedHeaders = $response.headers;
-modifiedHeaders['Location'] = decodeURIComponent(url);
-
-var modifiedStatus = 'HTTP/1.1 302 Found';
-
-$done({ status: modifiedStatus, headers: modifiedHeaders });
-// $done({headers : modifiedHeaders});
-// $done({}); // Not changed.
--- Reddit/pref.js
+++
@@ -1,7 +0,0 @@
-var obj = JSON.parse($response.body);
-obj['hide_ads'] = true;
-obj['third_party_site_data_personalized_ads'] = false;
-obj['third_party_personalized_ads'] = false;
-obj['activity_relevant_ads'] = false;
-obj['third_party_data_personalized_ads'] = false;
-$done({ body: JSON.stringify(obj) });
